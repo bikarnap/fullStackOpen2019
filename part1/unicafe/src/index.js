12f689c4dc79642e68bf5029d6ce3b0c1cb5c1b7
Bikarna Pokharel <bikarnapokharel@Bikarnas-MacBook-Pro.local>
2019-06-18 22:39:59 +0300

    initial commit to unicafe exercises

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = ({handleClick, text}) => 
+    <button onClick={handleClick}>{text}</button>
+
+const Statistics = ({feedback, value, unit}) => {
+    if(isNaN(value)) {
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <td width="70">{feedback}</td>
+                        <td>0 {unit}</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+    
+    
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td width="70">{feedback}</td>
+                    <td>{value} {unit}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+} 
+    
+    
+   
+    
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    console.log(good,bad,neutral)
+    const handleGoodClick = () => {
+        
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <h1>give Statistics</h1>
+            <div>
+                <Button handleClick={handleGoodClick} text='good' />
+                <Button handleClick={handleNeutralClick} text='neutral' />
+                <Button handleClick={handleBadClick} text='bad' />
+            </div>
+            
+            <h1>statistics</h1>
+
+            <div>
+                <Statistics feedback='good' value={good} unit='' />
+                <Statistics feedback='neutral' value={neutral} unit='' />
+                <Statistics feedback='bad' value={bad} unit='' />
+                <Statistics feedback='all' value={good + neutral + bad} unit='' />
+                <Statistics feedback='average' 
+                         value={(good-bad)/(good + neutral + bad)} unit='' />
+                <Statistics feedback='positive' 
+                         value={((good/(good + neutral + bad))*100)} unit=" %" />
+            </div>
+            
+            
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
