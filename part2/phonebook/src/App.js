da331268d4632a46590004e9f5f63f6c28b1e7a6
Bikarna Pokharel <bikarnapokharel@Bikarnas-MacBook-Pro.local>
2019-06-20 13:32:22 +0300

    step 1 completed. Now person names can be inserted using the form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,47 @@
 import React, { useState } from 'react';
 
+const Person = ({ person }) => <p>{person.name}</p>
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas', id: 1}
   ])
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('new name')
+
+  const names = () =>
+    persons.map(person => 
+      <Person 
+          key={person.id}
+          person={person}
+      />    
+  )
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+        name: newName,
+        id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+}
+
+  const handleNewName = (event) => setNewName(event.target.value)
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>{names()}</div>
     </div>
   )
 }
