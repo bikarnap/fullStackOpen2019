2b5fc7c684baeb46d5680ec2411d0ba79f492dd0
Bikarna Pokharel <bikarnapokharel@Bikarnas-MacBook-Pro.local>
2019-06-23 18:59:37 +0300

    refactored

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,64 +1,64 @@
 import React, { useState } from 'react';
-
-const Person = ({ person }) => <p>{person.name} {person.number}</p>
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {name: 'Arto Hellas', number: '123-456-789'}
+    {name: 'Arto Hellas', number: '123-456-789'}, 
+    {name: 'David Asphalt', number: '123-456-312'},
+    {name: 'Antti Hellas', number: '123-456-789'}, 
+    {name: 'Rafique Asphalt', number: '123-456-312'}
   ])
   const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNumber] = useState('')
+  const [ newNumber, setNumber ] = useState('')
+  const [ filteredName, setFilteredName ] = useState('')
 
-  const names = () =>
-    persons.map(person => 
-      <Person 
-          key={person.name}
-          person={person}
-      />    
-  )
-
+  const nameExists = (personObject) => {
+    let exists = false
+    for(let i = 0; i < persons.length; i++) {
+      if(persons[i].name === personObject.name) {
+        exists = true
+        break
+      }
+    }
+    if(!exists) {
+      setPersons(persons.concat(personObject))
+      exists = false
+    } else {
+      alert(`${newName} is already added to phonebook`)
+    }
+  }
+  
   const addNewPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
       number: newNumber
     }
-    let nameExists = false
-    for(let i = 0; i < persons.length; i++) {
-      if(persons[i].name === personObject.name){
-        nameExists = true
-        break
-      }
-    }
-    if(!nameExists) {
-      setPersons(persons.concat(personObject))
-      nameExists = false
-    } else {
-      alert(`${newName} is already added to phonebook`)
-    }
+    nameExists(personObject)
     setNewName('')
     setNumber('')
   }
 
   const handleNewName = (event) => setNewName(event.target.value)
   const handleNumber = (event) => setNumber(event.target.value)
+  const handleFilter = (event) => setFilteredName(event.target.value)
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-        number: <input value={newNumber} onChange={handleNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>{names()}</div>
+      <Filter search={filteredName} handleSearch={handleFilter} />
+      <h3>Add a new</h3>
+      <PersonForm addNewPerson={addNewPerson} 
+                  newName={newName}
+                  handleNewName={handleNewName}
+                  newNumber={newNumber}
+                  handleNewNumber={handleNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons} search={filteredName}/>
     </div>
   )
 }
