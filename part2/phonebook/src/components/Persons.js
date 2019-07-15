6dcd20cb79268318f6712156a780277dafd1bcfa
Bikarna Pokharel <bikarnapokharel@Bikarnas-MacBook-Pro.local>
2019-07-15 23:42:58 +0300

    search filter implemented using string method includes

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
 const Persons = ({ persons, search }) => {
-
-    
-
     const namesToShow = (search==='')
         ? persons
-        : persons.filter(person => person.name === search)
+        : persons.filter(person => person.name.includes(search))
 
     const names = () =>
-        namesToShow.map((person) => <p key={person.name}>{person.name} {person.number}</p> )
+        namesToShow.map((person) => 
+            <p key={person.name}>
+                {person.name} {person.number}
+            </p> 
+        )
    
     return(
       <div>
           {names()}
       </div>
-  )
-  
+    )
 }
-
-
 
 export default Persons
